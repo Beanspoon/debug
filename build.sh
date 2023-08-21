de0c8798e37fd4922b19d1ddaf67ae9d2d926139
Peter Kubiak <peter.s.kubiak@bath.edu>
2023-08-21 23:59:16 +0100

    Add clean and help options to build script

--- build.sh
+++ build.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+for arg in "$@"; do
+    case "$arg" in
+        -h|--help)
+            echo "Usage: build.sh [OPTIONS]"
+            echo "Options:"
+            echo "  -h, --help  Show this help message"
+            echo "  -c, --clean Perform a clean build"
+            exit
+            ;;
+        -c|--clean)
+            echo "Performing clean build..."
+            rm -rf build
+            ;;
+        *)
+            echo "Unknown option: $arg"
+            exit
+            ;;
+    esac
+done
+
 if [ ! -d "build" ]; then
-    echo "Creating build directory"
+    echo "Creating build directory..."
     mkdir build
 fi
 
