fd8ba38bf06ebfc9171835d6f427afcc78c34d73
Peter Kubiak <peter.s.kubiak@bath.edu>
2023-09-09 01:32:55 +0100

    Begin trace HAL

---
+++ chipsets/nrf52832/src/trace_hal.c
@@ -0,0 +1,16 @@
+#include "core_debug_reg.h"
+#include "core_itm_reg.h"
+#include "core_tpiu_reg.h"
+#include "clock_prv.h"
+
+void trace_init(void)
+{
+    CLOCK.TRACECONFIG.traceMux = CLOCK_TRACEMUX_SERIAL;
+    CLOCK.TRACECONFIG.tracePortSpeed = CLOCK_TRACEPORT_SPEED_32MHZ;
+
+    CORE_TPIU.TPIU_SPPR.TXMODE = TPIU_SPPR_TXMODE_SWO_MANCHESTER;
+    CORE_TPIU.TPIU_ACPR = 3332U;    // 9600 baud
+
+    CORE_ITM.ITM_TPR = TRACE_PRIVILEGE_PORT_0_TO_7_MASK;
+    
+}
