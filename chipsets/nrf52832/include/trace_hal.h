ca181e01364739b757545fa3608de59b385bcc38
Peter Kubiak <peter.s.kubiak@bath.edu>
2023-09-11 00:09:21 +0100

    Add header file for trace

---
+++ chipsets/nrf52832/include/trace_hal.h
@@ -0,0 +1,14 @@
+#ifndef TRACE_HAL_H
+#define TRACE_HAL_H
+
+/**
+ * @brief Initialises the trace functionality
+*/
+void trace_init(void);
+
+/**
+ * @brief Prints 'a'
+*/
+void print(void);
+
+#endif  // TRACE_HAL_H
